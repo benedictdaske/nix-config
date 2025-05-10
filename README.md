ee6cccf431722ae0e85d75a320d48a2cb4c98bac
brene-mbp <benedictdaske@tutanota.com>
2025-05-11 00:37:05 +0200

    wip flake.nix

--- README.md
+++ README.md
@@ -26,4 +26,4 @@ find apps/$(uname -m | sed 's/arm64/aarch64/')-darwin -type f \( -name apply -o 
 
 ## Authorship
 
-Part of this config was collected from other Nix configurations. Most notably, [dustinlyons](https://github.com/dustinlyons/nixos-config), []() has been a great but challenging introduction to Nix.
+Part of this config was collected from other Nix configurations. Most notably, the configs of [dustinlyons](https://github.com/dustinlyons/nixos-config), [mitchellh](https://github.com/mitchellh/nixos-config) and [shayne](https://github.com/shayne/nixos-config) have been a great but challenging introduction to Nix. Please give their configs a look, these people actually know what they're doing...
--- flake.nix
+++ flake.nix
@@ -2,10 +2,30 @@
     description = "Nix system configuration optimised for Darwin using Mac";
 
     inputs = {
-        
+        # primary nixpkgs repo
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+        # unstable repo for selected packages
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        # nix-darwin for MacOS
+        nix-darwin = {
+            url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+        homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
+        homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
+        homebrew-bundle = { url = "github:homebrew/homebrew-bundle"; flake = false; };
+
+        # home-manager for user config
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
     };
 
-    outputs = {
-
-    };
+    outputs = { nixpkgs, ... }@inputs:
 }
