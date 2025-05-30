260a54da1ea1dedbb68e5a2e21ef402543a68a2f
brene-mbp <benedictdaske@tutanota.com>
2025-05-31 01:11:38 +0200

    adapt systems/revival/config

--- systems/revival/darwin-configuration.nix
+++ systems/revival/darwin-configuration.nix
@@ -2,34 +2,14 @@
     nixpkgs.hostPlatform = "aarch64-darwin";
 
     # m2 macbook pro
-    networking.hostName = "revival";
-
-    # build environment
-    nix = {
-        enable = false;
-        settings = {
-            sandbox = "relaxed";
-            extra-sandbox-paths = [
-                "/private/var/db/oah" # aot files
-                "/Library/Apple" # rossetta runtime
-            ];
-            trusted-users = [ "@admin" ];
-        };
-    };
+    networking.hostName = "borderline";
 
     # enable touch id for sudo
-    # deprecated ?
-    # security.pam.enableSudoTouchIdAuth = false;
+
+    # security.pam.enableSudoTouchIdAuth = true;
     # security.pam.services.sudo_local.touchIdAuth = true;
 
-    # enable in case sandbox is too strict
-    # system.systemBuilderArgs = lib.mkIf (config.nix.settings.sandbox == "relaxed") {
-    #     sandboxProfile = ''
-    #         (allow file-read* file-write* process-exec mach-lookup (subpath "${builtins.storeDir}"))
-    #     '';
-    # };
-
-        # local nix-index, research this, how to use?
+    # local nix-index, research this, how to use?
     # programs.nix-index.enable = true;
 
     homebrew = {
@@ -48,7 +28,46 @@
         brews = [];
         taps = [];
         casks = [
-            "ghostty"
+            # "arc"
+            # "anytype"
+            # "audacity"
+            # "bambu-studio"
+            # "bartender"
+            # "bentobox"
+            # "chatgpt"
+            # "claude"
+            # "cursor"
+            # "deskpad"
+            # "discord"
+            # "diffusionbee"
+            # "google-chrome"
+            # "grammarly-desktop"
+            # "homerow"
+            # "inkscape"
+            # "iterm2"
+            # "linearmouse"
+            # "lunar"
+            # "microsoft-remote-desktop"
+            # "multipass"
+            # "ollama"
+            # "orbstack"
+            # "pearcleaner"
+            # "raycast"
+            # "screen-studio"
+            # "shortcat"
+            # "shureplus-motiv"
+            # "signal"
+            # "slack"
+            # "stats"
+            # "steam"
+            # "swiftformat-for-xcode"
+            # "syncthing"
+            # "tailscale"
+            # This freezes on quit, using desktop.telegram.org for now
+            # "telegram-desktop"
+            # "utm"
+            # "visual-studio-code"
+            # "vlc"
         ];
         # These app IDs are from using the mas CLI app
         # mas = mac app store
@@ -66,62 +85,5 @@
 
     system = {
         stateVersion = 6;
-
-        defaults = {
-
-            NSGlobalDomain = {
-                # 24-hour time
-                AppleICUForce24HourTime = true;
-                # dark mode
-                AppleInterfaceStyle = "Dark";
-                # file extensions
-                AppleShowAllExtensions = true;
-                # diasable for key-repeat
-                ApplePressAndHoldEnabled = false;
-
-                # fast key repeat
-                KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
-                InitialKeyRepeat = 25; # Values: 120, 94, 68, 35, 25, 15
-
-                # default: fn instead of actions
-                "com.apple.keyboard.fnState" = true;
-                # allow tap to click
-                "com.apple.mouse.tapBehavior" = 1;
-                # disable error sounds
-                "com.apple.sound.beep.volume" = 0.0;
-                "com.apple.sound.beep.feedback" = 0;
-            };
-
-            dock = {
-                autohide = true;
-                # magnified size
-                largesize = 48;
-                launchanim = true;
-                magnification = true;
-                minimize-to-application = true;
-                orientation = "bottom";
-                show-recents = false;
-                tilesize = 32;
-            };
-
-            finder = {
-                # TODO necessary ???
-                _FXShowPosixPathInTitle = true;
-                # enable column view for new windows
-                FXPreferredViewStyle = "clmv";
-                # bottom path bar
-                ShowPathbar = true;
-            };
-
-            loginwindow = {
-                # disable guest account
-                GuestEnabled = false;
-            };
-
-            trackpad = {
-                Clicking = true;
-                TrackpadThreeFingerDrag = true;
-            };
-        };
     };
 }
