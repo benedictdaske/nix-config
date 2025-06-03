dd30ac1c39cc79fa6a971987e1c85d51bcc426a9
brene-mbp <benedictdaske@tutanota.com>
2025-06-03 17:12:23 +0200

    add fish as default

--- systems/base/darwin-configuration.nix
+++ systems/base/darwin-configuration.nix
@@ -21,7 +21,10 @@
             SYSTEMD_EDITOR = "nvim";
             VISUAL = "nvim";
         };
-        shells = with pkgs; [ bashInteractive zsh ];
+        shells = with pkgs; [ bashInteractive zsh ]
+        ++ [
+            "${"/opt/homebrew/bin/fish"}"
+        ];
     };
 
     fonts = {
@@ -145,7 +148,7 @@
     # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
     users.users.brene = {
         home = "/Users/brene";
-        shell = pkgs.zsh;
+        shell = "/opt/homebrew/bin/fish";
     };
 
     # zsh is the default shell on Mac and we want to make sure that we're
